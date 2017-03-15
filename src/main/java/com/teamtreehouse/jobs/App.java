aabcdff06cf70a5359c123a152f8c52a3162ca3d
Craig Dennis <craig@teamtreehouse.com>
2017-03-15 16:12:52 -0700

    Take a peek!  #dadjoke

--- src/main/java/com/teamtreehouse/jobs/App.java
+++ src/main/java/com/teamtreehouse/jobs/App.java
@@ -40,12 +40,9 @@ public class App {
         .sorted()
         .collect(Collectors.toList());
 
-    int pageSize = 20;
-    int numPages = companies.size() / pageSize;
-
-    IntStream.iterate(1, i -> i + pageSize)
-        //.mapToObj(i -> String.format("%d. %s", i, companies.get(i)))
-        //.limit(numPages)
+    companies.stream()
+        .peek(company -> System.out.println("======>" + company))
+        .filter(company -> company.startsWith("N"))
         .forEach(System.out::println);
 
 
